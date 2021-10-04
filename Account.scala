70171a1a3903e856de763af4269917ddf3e2620e
Gabriel Alejandro Poma <67647491+gab77777@users.noreply.github.com>
2021-10-03 23:06:41 -0500

    Add files via upload
    Bank impl. Scala

---
+++ Account.scala
@@ -0,0 +1,49 @@
+package oobasics.bank
+
+class Account private(val customer: Customer, val id: String) {
+  private[this] var _balance = 0
+  
+  customer.addAccount(this)
+  
+  def balance = _balance
+  
+  def balance_=(newBalance: Int) = {
+    if (newBalance < _balance) withdraw(_balance - newBalance)
+    else deposit(newBalance - _balance)
+  }
+  
+  def deposit(amount: Int): Boolean = {
+    if(amount < 0) false
+    else {
+      _balance += amount
+      true 
+    }
+  }
+  
+  def withdraw(amount: Int): Boolean = {
+    if(amount < 0 || amount > _balance) false
+    else {
+      _balance -= amount
+      true
+    }
+  }
+}
+  
+object Account {
+  private var nextAccountNumber = 0
+    
+  def main(args: Array[String]): Unit = {
+    val a = new Account(new Customer("Mark", "Lewis", "id", new Address(Nil)), "id")
+    a.balance = 700
+    a.balance += 40
+  }
+    
+  def apply(c: Customer): Account = {
+    nextAccountNumber += 13
+    new Account(c, nextAccountNumber.toString)
+  }
+    
+  def apply(lines: Iterator[String]): Account = {
+    ???
+  }
+}
---
+++ Customer.scala
@@ -0,0 +1,28 @@
+package oobasics.bank
+
+class Customer(
+    val firstName: String,
+    val lastName: String,
+    val id: String,
+    private var _address: Address){
+  
+  private var _accounts: List[Account] = Nil
+  
+  def addAccount(account: Account): Unit = _accounts ::= account
+  
+  def removeAccount(id: String): Boolean = {
+    var index = _accounts.indexWhere(_.id == id)
+    if(index < 0) false
+    else {
+      _accounts = _accounts.patch(index, Nil, 1)
+      true
+    }
+  }
+  
+  def accounts = _accounts
+  
+  def changeAddress(a: Address): Unit = _address = a
+  
+  def address = _address
+  
+}
